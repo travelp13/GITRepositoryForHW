3dfd16593459a6fa570e3bd7ab96d897fc76469a
ptre <ptre@diceus.com>
2024-09-24 03:34:17 +0200

    Changes in HW7.py file

--- Essential_HW7/HW7.py
+++ Essential_HW7/HW7.py
@@ -8,9 +8,9 @@ my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for i in my_generator(my_list):
     print(i)
 
-# Task2
-# Виведіть із списку чисел список квадратів парних чисел. 
-# Використовуйте 2 варіанти рішення: генератор та цикл
+# # Task2
+# # Виведіть із списку чисел список квадратів парних чисел. 
+# # Використовуйте 2 варіанти рішення: генератор та цикл
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 def my_generator(lst: list):
@@ -31,3 +31,44 @@ for num in my_list:
 
 print(result_list)
 
+# Task3
+# Напишіть функцію-генератор для отримання n перших простих чисел.
+def get_simple_num():
+    numbers = list(range(2,10001))
+    simple_numbers = []
+
+    for number in numbers:
+        if number in (2, 3, 5, 7):
+            simple_numbers.append(number)
+        elif number > 10 and number%10 in (1, 3, 7, 9):
+            if number%3 != 0 and number%7 !=0 and number%number**0.5 !=0:
+                simple_numbers.append(number)
+
+    notsimple_numbers = []
+    rng = round(len(simple_numbers)/2)+1
+
+    for number in simple_numbers:
+        for not_simple in simple_numbers[0:rng]:
+            if number%not_simple == 0 and number != not_simple:
+                notsimple_numbers.append(number)
+
+    for number in simple_numbers:
+        if number in notsimple_numbers:
+            simple_numbers.remove(number)
+    
+    return simple_numbers
+
+simple_numbers = get_simple_num()
+
+def my_generator(n):
+    number = 0
+    count = 0
+    while count < n:
+        if number in simple_numbers:
+            yield number
+            count += 1
+        number += 1
+
+n = 20
+for simple_number in my_generator(n):
+    print(simple_number)
